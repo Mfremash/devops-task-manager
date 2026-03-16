26df1368d9f71942a1bddeeb21971a082f8803dd
Aktan Sepbaev <mfremash@MacBook-Pro-Aktan.local>
2026-03-16 09:32:19 +0500

    Convert frontend from submodule to regular directory

---
+++ frontend/src/App.js
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+
+function App() {
+
+  const [tasks, setTasks] = useState([]);
+  const [title, setTitle] = useState("");
+
+  const API = "/api/tasks";
+
+  const fetchTasks = async () => {
+    const res = await fetch(API);
+    const data = await res.json();
+    setTasks(data);
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const addTask = async () => {
+    await fetch(API, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ title })
+    });
+
+    setTitle("");
+    fetchTasks();
+  };
+
+  return (
+    <div style={{padding:"40px"}}>
+      <h1>Task Manager</h1>
+
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="New task"
+      />
+
+      <button onClick={addTask}>
+        Add
+      </button>
+
+      <ul>
+        {tasks.map(task => (
+          <li key={task.id}>
+            {task.title}
+          </li>
+        ))}
+      </ul>
+
+    </div>
+  );
+}
+
+export default App;
